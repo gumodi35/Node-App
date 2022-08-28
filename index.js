85dc8c28e2bd57c9ae87ea2fb28b9e3a51417f2d
gumodi10@gmail.com <gumodi10@gmail.com>
2022-08-27 22:39:07 -0500

    add working directories

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 // Express application 
 import express from 'express';
 
+import { fileURLToPath } from 'url';
+import { dirname, sep } from 'path';
 
+// configuration 
+const 
+    __dirname = dirname(fileURLToPath( import.meta.url )) + sep,
+    cfg = {
+        port: process.env.PORT || 3000,
+        dir: {
+            root: __dirname,
+            static: __dirname + 'static' +sep
+        }
+    };
+
+console.dir(cfg, { depth: null, color: true });
+
+// Express initiation
+// ...rest of code
 
 // configuration
-const 
+/* const 
     cfg = {
         port: process.env.PORT || 3000
     };
-
+ */
 // Express initiation
 const app = express();
 
@@ -29,10 +46,12 @@ app.get('/hello/', (req, res) => {
 });
 
 // serve static assets
-app.use(express.static( 'static' ));
+app.use(express.static( cfg.dir.static ));
 
 // start server
 app.listen(cfg.port, () => {
     console.log(`Example app listening at http://localhost:${ cfg.port }`);
 });
 
+// export defaults
+export { cfg, app}
